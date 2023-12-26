56bf9f3ddcfa88e4da3017f0d86ff7572a813b93
Andres Valencia <andres228cv@gmail.com>
2023-12-26 16:23:39 -0500

    Refactor generate_missions_file and make 2 test for this function

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import configparser
 import schedule
 import time
-import random
 from proyecto_final.DEVICES.generate_mission_files import Generate_Files
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-num_files_initial = config.get('general', 'num_files_initial')
-num_files_final = config.get('general', 'num_files_final')
-time_cycle_config = config.get('general', 'time_cycle')
+
+num_files_initial = config.getint('general', 'num_files_initial')
+num_files_final = config.getint('general', 'num_files_final')
+time_cycle_config = config.getint('general', 'time_cycle')
 
 print("Script Start")
 generate_files_instance = Generate_Files()
 
 def file_generator():
-    print("Script Job")
-    generate_files_instance.generate_files(num_files_initial,num_files_final)
+    print("Job function")
+    generate_files_instance.generate_files(num_files_initial, num_files_final)
 
-    def main():
-     schedule.every(time_cycle_config).minutes.do(file_generator)
 
-     while True:
-        print("¡Testing schedule!")
+def main():
+    print("main")
+    schedule.every(time_cycle_config).seconds.do(file_generator)
+
+    while True:
         schedule.run_pending()
         time.sleep(1)
 
 
-    if __name__ == "__main__":
-     main()
+if __name__ == "__main__":
+ main()
--- proyecto_final/DEVICES/generate_mission_files.py
+++ proyecto_final/DEVICES/generate_mission_files.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 from .functions import date_format, hash_format
 from .classes import Mission, Devices, Status
 
@@ -10,40 +11,62 @@ class Generate_Files:
         self.status_instance = Status()
         self.hash_format = hash_format
         self.date_format = date_format
-        self.file_number = 0
 
-    def create_output_directory(self, num_folder):
+    def create_output_directory(self, num_folder :int,times_stamp):
+        """This method will create a directory in the correct path
+        :param num_folder: folder number
+        :type num_folder: int
+        :param times_stamp: unique id for the folder
+        :return: will return the output_directory path
+        :rtype: string
+        """
         current_directory = os.path.dirname(os.path.abspath(__file__))
-        output_directory = os.path.join(current_directory, f"SIMULATIONS/Folder-{num_folder}")
+        output_directory = os.path.join(current_directory, f"SIMULATIONS/Folder-{num_folder}-{times_stamp}")
+
         os.makedirs(output_directory, exist_ok=True)
         return output_directory
 
     def generate_file_data(self):
+        """The method will return the data to create the files
+        :return: random data
+        """
         return {
             "name": random.choice(self.mission_instance.mission),
             "component": random.choice(self.devices_instance.device),
             "status": random.choice(self.status_instance.status),
         }
 
-    def generate_files(self, num_files_from, num_files_to):
-        num_folder = num_files_to - num_files_from
-        output_directory = self.create_output_directory(num_folder)
+    def generate_files(self, num_files_from:int, num_files_to:int):
+        """This function takes create_output_directory to create folders
+        and put them in the wanted path and the function generate_file_data
+        to get the necessary info for the creation of the files
 
-        all_files_data = [self.generate_file_data() for _ in range(num_files_from, num_files_to)]
+        :param num_files_from: the start number for the range of numbers
+        :type num_files_from: int
+        :param num_files_to: the final number for the range of numbers
+        :type num_files_to: int
+        """
+        timestamp = int(time.time())
+        num_random = random.randrange(num_files_from,num_files_to)
+        output_directory = self.create_output_directory(num_random,timestamp)
+
+        # This data structure generates the data as many times required for each file
+        all_files_data = [self.generate_file_data() for _ in range(num_random)]
+        self.file_number = 0
 
         for file_data in all_files_data:
-            self.file_number += 1
-            name, component, status = file_data["name"], file_data["component"], file_data["status"]
-            date_time = self.date_format()
-            hash_m = self.hash_format(date_time, name, component, status)
+         self.file_number += 1
+         name, component, status = file_data["name"], file_data["component"], file_data["status"]
+         date_time = self.date_format()
+         hash_m = self.hash_format(date_time, name, component, status)
 
-            if name == "UNKN":
+         if name == "UNKN":
                 name, component, status, hash_m = "Unknown", "Unknown", "Unknown", ""
 
-            file_name = f"APL[{name}]-0000[{self.file_number}].log"
-            file_path = os.path.join(output_directory, file_name)
+         file_name = f"APL[{name}]-0000[{self.file_number}].log"
+         file_path = os.path.join(output_directory, file_name)
 
-            with open(file_path, 'w') as file:
+         with open(file_path, 'w') as file:
                 file.write(f"Date: {date_time}\n")
                 file.write(f"Mission: {name}\n")
                 file.write(f"Device Type: {component}\n")
@@ -51,3 +74,6 @@ class Generate_Files:
                 file.write(f"Hash: {hash_m}\n")
 
 
+#instancia = Generate_Files()
+#if __name__ == "__main__":
+ #instancia.generate_files(5, 12)
