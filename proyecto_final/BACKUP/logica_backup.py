2da2c3afbffcfdf1b36f2ad8f2a567dfd43d17e3
Miguel Angel Mulcue Restrepo <miguel.mulcue@sura-am.com>
2024-01-18 23:15:45 -0500

    Se modifica la lógica de backup para que no elimine las simulaciones si no que tambien las pase, además se crear dos carpertas en BACKUP que se llaman Reportes y Simulaciones, estas almacenan la información bajo un identificador proporcionado por la fecha y hora

--- proyecto_final/BACKUP/logica_backup.py
+++ proyecto_final/BACKUP/logica_backup.py
@@ -2,66 +2,88 @@ import os
 import shutil
 from datetime import datetime
 
-def delete_simulation_files(simulations_path):
-    """
-    Delete all files within the SIMULATIONS folder.
+# Source and destination paths
+reports_directory_path = r'C:\Users\miguel.mulcue\OneDrive - Inversiones Internacionales Grupo Sura S.A\Escritorio\Pyhton_RutaN\proyecto_final\DEVICES\REPORTS'
+simulations_directory_path = r'C:\Users\miguel.mulcue\OneDrive - Inversiones Internacionales Grupo Sura S.A\Escritorio\Pyhton_RutaN\proyecto_final\DEVICES\SIMULATIONS'
+backUps_reports_directory_path = r'C:\Users\miguel.mulcue\OneDrive - Inversiones Internacionales Grupo Sura S.A\Escritorio\Pyhton_RutaN\proyecto_final\BACKUPS\REPORTS'
+backUps_simulations_directory_path = r'C:\Users\miguel.mulcue\OneDrive - Inversiones Internacionales Grupo Sura S.A\Escritorio\Pyhton_RutaN\proyecto_final\BACKUPS\SIMULATIONS'
 
-    :param simulations_path: Path to the SIMULATIONS folder.
-    :type simulations_path: str
-    """
-    # Delete all files within SIMULATIONS
-    for folder in os.listdir(simulations_path):
-        folder_path = os.path.join(simulations_path, folder)
-        try:
-            shutil.rmtree(folder_path)
-        except Exception as e:
-            print(f"Error deleting folder {folder_path}: {e}")
 
-def move_files(source_path, destination_path):
+current_date = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+def move_files_reports(source_path=reports_directory_path, destination_path=backUps_reports_directory_path, name = current_date):
     """
-    Move files starting with 'APLSTATS' from source to destination folder.
+    Move all reports from source to destination folder.
 
     :param source_path: Path to the source folder.
     :type source_path: str
     :param destination_path: Path to the destination folder.
     :type destination_path: str
+    :param name: Unique identifier created with the current date and time.
+    :type name: str
     """
-    # Get the list of files in the source folder
+    # Get the list of all files in the source folder
     files = os.listdir(source_path)
 
-    # Filter files that start with "APLSTATS"
-    aplstats_files = [file for file in files if file.startswith("APLSTATS")]
-
     try:
-        # If no 'APLSTATS' files are found, raise an exception
-        if not aplstats_files:
-            raise FileNotFoundError("No files with the name 'APLSTATS' were found.")
+        # If no files are found, raise an exception
+        if not files:
+            raise FileNotFoundError("No files were found in the source folder.")
 
         # Create a folder with the current date in the format YYYYMMDD
-        current_date = datetime.now().strftime("%Y%m%d-%H%M%S")
-        destination_folder = os.path.join(destination_path, current_date)
+        destination_folder = os.path.join(destination_path, name)
 
         if not os.path.exists(destination_folder):
             os.makedirs(destination_folder)
 
         # Move each file to the destination folder
-        for file in aplstats_files:
+        for file in files:
             source_file_path = os.path.join(source_path, file)
             destination_file_path = os.path.join(destination_folder, file)
             try:
                 shutil.move(source_file_path, destination_file_path)
-                print(f"File {file} moved to {destination_file_path}")
             except Exception as e:
                 print(f"Error moving file {source_file_path} to {destination_file_path}: {e}")
 
     except FileNotFoundError as e:
         print(e)
-        
-# Source and destination paths
-source_path = os.path.abspath(r'Pyhton_RutaN\proyecto_final\DEVICES\REPORTS')
-destination_folder = os.path.abspath(r'Pyhton_RutaN\proyecto_final\BACKUP')
-simulations_path = os.path.abspath(r'Pyhton_RutaN\proyecto_final\DEVICES\SIMULATIONS')
 
+def move_files_simulations(source_path=simulations_directory_path, destination_path=backUps_simulations_directory_path, name = current_date):
+    """
+    Move all simulations from source to destination folder.
 
-delete_simulation_files(simulations_path)
-move_files(source_path, destination_folder) 
+    :param source_path: Path to the source folder.
+    :type source_path: str
+    :param destination_path: Path to the destination folder.
+    :type destination_path: str
+    :param name: Unique identifier created with the current date and time.
+    :type name: str
+    """
+    # Get the list of all files in the source folder
+    files = os.listdir(source_path)
+
+    try:
+        # If no files are found, raise an exception
+        if not files:
+            raise FileNotFoundError("No files were found in the source folder.")
+
+        # Create a folder with the current date in the format YYYYMMDD
+        destination_folder = os.path.join(destination_path, name)
+
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+
+        # Move each file to the destination folder
+        for file in files:
+            source_file_path = os.path.join(source_path, file)
+            destination_file_path = os.path.join(destination_folder, file)
+            try:
+                shutil.move(source_file_path, destination_file_path)
+            except Exception as e:
+                print(f"Error moving file {source_file_path} to {destination_file_path}: {e}")
+
+    except FileNotFoundError as e:
+        print(e)
+
+move_files_reports()
+move_files_simulations()
